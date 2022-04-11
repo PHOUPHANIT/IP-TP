b44599aa12ab354852b40ecee5f416ebe1201fb8
Teng phouphanit <phouphanitteng@gmail.comm>
2022-04-11 10:59:00 +0700

    Finish exercise-2 TP12

--- routes/price.js
+++ routes/price.js
@@ -5,6 +5,12 @@ const { } = require('../schemas');
 var router = express.Router();
 const priceService = require('../services/price');
 
+router.get('/id/:id',auth.ensureSignedIn, async function(req,res,next){
+  const {id} = req.params
+  const result = await priceService.findById(id)
+  res.json(result)
+})
+
 router.post('/create', auth.ensureSignedIn, async (req, res) => {
   const { product, price, source } = req.body;
   const result = await priceService.create({ product, price, source })
@@ -18,12 +24,14 @@ router.get('/all', auth.ensureSignedIn, async (req, res) => {
 
 router.post('/update', auth.ensureSignedIn, async (req, res, next) => {
   // to do
-  res.json({});
+  const updatePrice = await priceService.update(req.body);
+  res.json(updatePrice);
 })
 
 router.post('/delete', auth.ensureSignedIn, async (req, res, next) => {
   // to do
-  res.json({});
+  const result = await priceService.remove(req.body);
+  res.json(result);
 })
 
 module.exports = router
--- services/price.js
+++ services/price.js
@@ -1,5 +1,13 @@
 const Prices = require("../models/prices");
 
+const findById = async (id) =>{
+  const price = await Prices.findById(id)
+  return{
+      success:true,
+      data:price
+  }
+}
+
 const create = async (newPrice) => {
   // to do
   const createdPrice = await Prices.create(newPrice);
@@ -14,15 +22,41 @@ const findAll = async () => {
   }
 }
 
-const update = async () => {
+const update = async (params) => {
   // to do
+  try{
+    const {id, price, source} = params;
+    const update= await Prices.updateOne({_id:id},{price:price, source:source});
+    return {
+      success: true,
+      data: update
+    }
+  }catch(err){
+    return{
+      success: false,
+      error: err
+    }
+  }
 }
 
-const remove = async () => {
+const remove = async (id) => {
   // to doF
+  try{
+    const deletePrice = await Prices.deleteOne(id);
+    return {
+      success: true,
+      data: deletePrice
+    }
+  }catch(err){
+    return {
+      success: false,
+      error: err
+    }
+  }
 }
 
 module.exports = {
+  findById,
   update,
   remove,
   findAll,
