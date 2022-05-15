f2d1266d8848f9c6104d70caf0c4f329b0074c56
Teng phouphanit <phouphanitteng@gmail.comm>
2022-05-15 22:35:44 +0700

    Finish TP13

---
+++ Client/src/views/HomeLogin.vue
@@ -0,0 +1,10 @@
+<script setup>
+import LoginPage from "@/components/LoginPage.vue";
+// import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <LoginPage />
+  </main>
+</template>
---
+++ Client/src/views/HomeSignup.vue
@@ -0,0 +1,10 @@
+<script setup>
+import SignupPage from "@/components/SignupPage.vue";
+// import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <SignupPage />
+  </main>
+</template>
